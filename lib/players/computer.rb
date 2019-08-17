f30a0e0093003ab06fe84ab739f6720a1081070b
Amanda King <amapickell@gmail.com>
2019-08-17 16:57:52 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,7 +6,7 @@ module Players
     
     def move (board)
       #move returns a valid position for the computer to move
-      move = 5.to_s
+      move = "5"
       until !board.taken?(move)
         move = rand(1..9).to_s
       end
