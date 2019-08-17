21386303e9748cebe9b9eb85d30379cf463f63c7
Amanda King <amapickell@gmail.com>
2019-08-17 17:30:19 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,18 +6,19 @@ module Players
     
     def move (board)
       #move returns a valid position for the computer to move
-      move = "5"
-      until !board.taken?(move)
-        move = rand(1..9).to_s
-      end
-      move
+      #move = "5"
+      #until !board.taken?(move)
+        #move = rand(1..9).to_s
+      #end
+      #move
       
       ["5", "1", "3", "7", "9"].reverse.each do |number|
         if !board.taken?(number)
           move = number
         else
-          
+          move = rand(1..9).to_s
         end
+        move
       end
     end
     
